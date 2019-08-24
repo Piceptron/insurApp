368d0582cdca239ffed00816b6b72b47670f10ae
Jarrod Servilla <jarrod.servilla@gmail.com>
2019-08-24 17:16:36 +0200

    Added base login screen

--- app/src/main/java/com/example/android/microinsurance/login/LoginActivity.java
+++ app/src/main/java/com/example/android/microinsurance/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.microinsurance.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.android.microinsurance.R;
+import com.example.android.microinsurance.navigation.NavigationActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToNav(View view) {
+        Intent intent = new Intent(this, NavigationActivity.class);
+        startActivity(intent);
     }
 }
